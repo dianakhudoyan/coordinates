74018585e2bf609226e9f7ac919e688d88b768c6
dianakhudoyan <dianaxudoyan9@gmail.com>
2024-10-23 20:14:40 +0400

    new

--- script.js
+++ script.js
@@ -15,3 +15,17 @@
   ctx.moveTo(250, 0);  
   ctx.lineTo(250, 500);  
   ctx.stroke();
+
+  var centerX = 150; 
+  var centerY = 100; 
+  var radius =40;  
+
+  ctx.beginPath(); 
+  ctx.arc(centerX, centerY, radius, 0, Math.PI * 2); 
+  ctx.fillStyle = 'black';
+  ctx.fill(); 
+  ctx.strokeStyle = 'black'; 
+  ctx.lineWidth = 1; 
+  ctx.stroke(); 
+
+  
