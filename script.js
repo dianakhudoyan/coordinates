f3869d04b375852462c5d3ff51d6b8f2204c0780
dianakhudoyan <dianaxudoyan9@gmail.com>
2024-10-23 23:32:29 +0400

    new

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 
   var canvas = document.getElementById('myCanvas');
   var ctx = canvas.getContext('2d');
-
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 2;
-
-  ctx.beginPath();
-  ctx.moveTo(0, 250);  
-  ctx.lineTo(500, 250);  
-  ctx.stroke();
-
   
-  ctx.beginPath();
-  ctx.moveTo(250, 0);  
-  ctx.lineTo(250, 500);  
-  ctx.stroke();
 
-  var centerX = 150; 
-  var centerY = 100; 
-  var radius =10;  
+  function drawC(){
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
 
-  ctx.beginPath(); 
-  ctx.arc(centerX, centerY, radius, 0, Math.PI * 2); 
-  ctx.fillStyle = 'black';
-  ctx.fill(); 
-  ctx.strokeStyle = 'black'; 
-  ctx.lineWidth = 1; 
-  ctx.stroke(); 
+    ctx.beginPath();
+    ctx.moveTo(0, 250);  
+    ctx.lineTo(500, 250);  
+    ctx.stroke();
 
+    
+    ctx.beginPath();
+    ctx.moveTo(250, 0);  
+    ctx.lineTo(250, 500);  
+    ctx.stroke();
+  }
+  
+ 
+
+   let data = {
+    centerX : 150, 
+    centerY : 100,
+    radius :10, 
+   }
+   let x=10
+   function upDate(){
+   
+    if(data.centerX+data.radius > canvas.width || data.centerX<0){
+      x*=-1
+    }
+    data.centerX+=x
+   }
+   
+  function draw(){
+    ctx.beginPath(); 
+    ctx.arc(data.centerX, data.centerY, data.radius, 0, Math.PI * 2); 
+    ctx.fillStyle = 'black';
+    ctx.fill(); 
+    ctx.strokeStyle = 'black'; 
+    ctx.lineWidth = 1; 
+    ctx.stroke(); 
+  }
+  function loop(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    requestAnimationFrame(loop)
+    drawC();
+    upDate();
+    draw()
+  }
+  loop()
   
