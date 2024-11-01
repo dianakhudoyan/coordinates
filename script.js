e1ed58598942144ba8531698067534d1fb67409e
dianakhudoyan <dianaxudoyan9@gmail.com>
2024-11-01 22:17:17 +0400

    js

--- script.js
+++ script.js
@@ -153,68 +153,65 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
 let data = {
-  centerX: 150,
-  centerY: 100 ,
-  radius: 10
+    centerX: 150,
+    centerY: 100,
+    radius: 10
 };
 
 let x = 1;
 let y = 1;
-let speed = 1 ; 
+let speed = 1;
 
 let squares = [
-  { x: 50, y: 50, size: 300 } 
+    { x: 50, y: 50, size: 300 } 
 ];
 
+let squareDirection = 0; 
+let squareSpeed = 2;
+let squarePadding = 30; 
+
 function checkCollision(circle, square) {
-  let circleLeft = circle.centerX - circle.radius;
-  let circleRight = circle.centerX + circle.radius;
-  let circleTop = circle.centerY - circle.radius;
-  let circleBottom = circle.centerY + circle.radius;
+    let circleLeft = circle.centerX - circle.radius;
+    let circleRight = circle.centerX + circle.radius;
+    let circleTop = circle.centerY - circle.radius;
+    let circleBottom = circle.centerY + circle.radius;
 
-  let squareLeft = square.x;
-  let squareRight = square.x + square.size;
-  let squareTop = square.y;
-  let squareBottom = square.y + square.size;
+    let squareLeft = square.x;
+    let squareRight = square.x + square.size;
+    let squareTop = square.y;
+    let squareBottom = square.y + square.size;
 
-  if (
-    circleRight > squareLeft &&
-    circleLeft < squareRight &&
-    circleBottom > squareTop &&
-    circleTop < squareBottom
-  ) {
-    let overlapX = Math.min(circleRight - squareLeft, squareRight - circleLeft);
-    let overlapY = Math.min(circleBottom - squareTop, squareBottom - circleTop);
+    if (
+        circleRight > squareLeft &&
+        circleLeft < squareRight &&
+        circleBottom > squareTop &&
+        circleTop < squareBottom
+    ) {
+        let overlapX = Math.min(circleRight - squareLeft, squareRight - circleLeft);
+        let overlapY = Math.min(circleBottom - squareTop, squareBottom - circleTop);
 
-    if (overlapX < overlapY) {
-      if (circleLeft < squareLeft) {
-        circle.centerX = squareLeft - circle.radius;
-      } else {
-        circle.centerX = squareRight + circle.radius;
-      }
-      x *= -1; 
-    } else {
-      if (circleTop < squareTop) {
-        circle.centerY = squareTop - circle.radius;
-      } else {
-        circle.centerY = squareBottom + circle.radius;
-      }
-      y *= -1;
+        if (overlapX < overlapY) {
+            if (circleLeft < squareLeft) {
+                circle.centerX = squareLeft - circle.radius;
+            } else {
+                circle.centerX = squareRight + circle.radius;
+            }
+            x *= -1;
+        } else {
+            if (circleTop < squareTop) {
+                circle.centerY = squareTop - circle.radius;
+            } else {
+                circle.centerY = squareBottom + circle.radius;
+            }
+            y *= -1;
+        }
+        return true;
     }
-    return true;
-  }
-  return false;
+    return false;
 }
 
-
-let squarePadding = 30; 
-squares[0].x = squarePadding; 
-squares[0].y = squarePadding;
-
-let squareDirectionX = 1; 
-let squareDirectionY = 1; 
-
 function update() {
+
     if (data.centerX + data.radius > canvas.width || data.centerX - data.radius < 0) {
         x *= -1;
     }
@@ -224,47 +221,56 @@ function update() {
     data.centerX += x * speed;
     data.centerY += y * speed;
 
-    squares[0].x += 2 * squareDirectionX;
-    if (squares[0].x + squares[0].size + squarePadding > canvas.width || squares[0].x < squarePadding) {
-        squareDirectionX *= -1; 
+    switch (squareDirection) {
+        case 0: 
+            squares[0].x -= squareSpeed;
+            if (squares[0].x < squarePadding) {
+                squareDirection = 1;
+            }
+            break;
+        case 1:
+            squares[0].x += squareSpeed;
+            if (squares[0].x + squares[0].size + squarePadding > canvas.width) { 
+                squareDirection = 2; 
+            }
+            break;
+        case 2: 
+            squares[0].y -= squareSpeed;
+            if (squares[0].y < squarePadding) { 
+                squareDirection = 3; 
+            }
+            break;
+        case 3: 
+            squares[0].y += squareSpeed;
+            if (squares[0].y + squares[0].size + squarePadding > canvas.height) {
+                squareDirection = 0;
+            }
+            break;
     }
 
-    squares[0].y += 2 * squareDirectionY;
-    if (squares[0].y + squares[0].size + squarePadding > canvas.height || squares[0].y < squarePadding) {
-        squareDirectionY *= -1; 
-    }
+    ժ
     squares.forEach(square => checkCollision(data, square));
 }
 
 function draw() {
- 
-  ctx.beginPath();
-  ctx.arc(data.centerX, data.centerY, data.radius, 0, Math.PI * 2);
-  ctx.fillStyle = 'black';
-  ctx.fill();
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 1;
-  ctx.stroke();
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    ctx.beginPath();
+    ctx.arc(data.centerX, data.centerY, data.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.stroke();
 
- 
-  squares.forEach(square => {
-    ctx.fillRect(square.x, square.y, square.size, square.size);
-  });
+    squares.forEach(square => {
+        ctx.fillRect(square.x, square.y, square.size, square.size);
+    });
 }
 
 function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  update();
-  draw();
-  requestAnimationFrame(loop);
+    update();
+    draw();
+    requestAnimationFrame(loop);
 }
-
-let speedControl = document.getElementById('speedControl');
-let speedValue = document.getElementById('speedValue');
-
-speedControl.addEventListener('input', function() {
-  speed = Number(speedControl.value); 
-  speedValue.textContent = speed;
-});
 
 loop();
